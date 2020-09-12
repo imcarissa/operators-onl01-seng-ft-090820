c57e6b04445b615c6005d23171c4d67cd4ef8df1
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 13:43:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return "true"
+  else
+    return "un_safe"
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed = 79
+	speed > 60 ? "true" : "false"
 end
 	
 
